11d1b0d58350586783be4ab33efd522c914da61a
wosmo <m.wasifmalik17@gmail.com>
2023-03-10 07:04:19 +0500

    made react custamized setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 
 
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Textbox from './components/Textbox';
-
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Textbox from './components/Textbox';
+import Counter from './TextBox/Counter'
 
 
 function App() {
   return (
-    <Textbox/>
+    <Counter />
   )
 }
 
