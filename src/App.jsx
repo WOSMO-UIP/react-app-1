f5b1ccdfa239f3a290624c273bb22d33f7598709
Muhammad Wasif Malik <m.wasifmalik17@gmail.com>
2023-03-11 11:21:02 +0500

    0_0

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,26 @@
-
-
 // import React from 'react'
 // import Counter from './TextBox/Counter'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import Counter from './TextBox/Counter'
-
 
 // function App() {
 //     return (
 //         <Counter heading="Text box" btn="click me" comment="i am a comment" />
 //       )
 //     }
-    
+
 //     export default App
-    
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Counter from './TextBox/Counter'
 
-
-import React, { Component } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+// import Counter from "./TextBox/Counter";
+import BtnCounter from "./codeStoicPractice/BtnCounter";
+import React, { Component } from "react";
 
 export default class App extends Component {
-  render   () {
-    return(
-          <Counter heading="Text box" btn="click me" comment="i am a comment" />  
-    )
+  render() {
+    return (
+      < BtnCounter />
+      // <Counter heading="Text box" btn="click me" comment="i am a comment" />
+    );
   }
 }
